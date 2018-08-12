ea3d70417cc2e222438de994b6ddd00457a8afae
BOT-Man-JL <g123nn@hotmail.com>
2018-08-12 16:24:53 +0800

    Update index layout

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ const app = getApp()
 Page({
   data: {
     articleList: [{
-      id: '1234',
+      id: '1',
       articleType: 'text',
       authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
       authorName: 'John Lee',
@@ -19,22 +19,7 @@ Page({
       fireCount: 998,
       commentCount: 998
     }, {
-      id: '12345',
-      articleType: 'video',
-      authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
-      authorName: 'John Lee',
-      timestamp: '一小时前',
-      location: '知春路',
-      articleData: {
-        text: '十号线地铁里竟然有个女生在抽烟，有个大爷提醒她，女生居然还大骂大爷...大家怎么看？十号线地铁里竟然有个女生在抽烟，大家怎么看？',
-        video: {
-          url: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg'
-        }
-      },
-      fireCount: 998,
-      commentCount: 998
-    }, {
-      id: '12345',
+      id: '2',
       articleType: 'picture',
       authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
       authorName: 'John Lee',
@@ -43,13 +28,13 @@ Page({
       articleData: {
         text: '十号线地铁里竟然有个女生在抽烟，有个大爷提醒她，女生居然还大骂大爷...大家怎么看？十号线地铁里竟然有个女生在抽烟，大家怎么看？',
         picture: {
-          url: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg'
+          src: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg'
         }
       },
       fireCount: 998,
       commentCount: 998
     }, {
-      id: '12345',
+      id: '3',
       articleType: 'pictures',
       authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
       authorName: 'John Lee',
@@ -58,26 +43,44 @@ Page({
       articleData: {
         text: '十号线地铁里竟然有个女生在抽烟，有个大爷提醒她，女生居然还大骂大爷...大家怎么看？十号线地铁里竟然有个女生在抽烟，大家怎么看？',
         pictures: [{
-          url: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
+          src: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
         }, {
-          url: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
+          src: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
         }, {
-          url: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
+          src: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
         }]
       },
       fireCount: 998,
       commentCount: 998
     }, {
-      id: '12345',
+      id: '4',
+      articleType: 'video',
+      authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
+      authorName: 'John Lee',
+      timestamp: '一小时前',
+      location: '知春路',
+      articleData: {
+        text: '十号线地铁里竟然有个女生在抽烟，有个大爷提醒她，女生居然还大骂大爷...大家怎么看？十号线地铁里竟然有个女生在抽烟，大家怎么看？',
+        video: {
+          poster: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
+          src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+        }
+      },
+      fireCount: 998,
+      commentCount: 998
+    }, {
+      id: '5',
       articleType: 'link',
       authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
       authorName: 'John Lee',
       timestamp: '一小时前',
       location: '知春路',
       articleData: {
-        thumb: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
-        abstractText: '从少年天才到大器晚成，为什么他才是阿森纳最佳？从少年天才到大器晚成，为什么他才是阿森',
-        text:'火箭少女空降希格玛！和每一位小姐姐握了手超开心了！火箭少女空降希格玛！！'
+        text: '火箭少女空降希格玛！和每一位小姐姐握了手超开心了！火箭少女空降希格玛！！',
+        link: {
+          thumb: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4',
+          abstractText: '从少年天才到大器晚成，为什么他才是阿森纳最佳？从少年天才到大器晚成，为什么...'
+        }
       },
       fireCount: 998,
       commentCount: 998
