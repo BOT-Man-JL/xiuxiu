0ae9b0378c81983df4f99e811de9c18c381dbda8
TianZhang <690725962@qq.com>
2018-08-13 17:52:11 +0800

    Update mock-data.js
    update content, author -info  
    test id rank

--- resources/home/mock-data.js
+++ resources/home/mock-data.js
@@ -4,20 +4,23 @@ export default {
     id: '1',
     articleType: 'text',
     metaData: {
-      authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
-      authorName: 'John Lee',
+      authorAvatar: 'http://pic.qqtn.com/up/2017-11/2017112315111758254.jpg',
+      authorName: '不吃鱼的猫',
       timestamp: '一小时前',
       location: '知春路'
     },
     articleData: {
-      text: '十号线地铁里竟然有个女生在抽烟，有个大爷提醒她，女生居然还大骂大爷...大家怎么看？十号线地铁里竟然有个女生在抽烟，大家怎么看？'
+      text: '大雨滂沱之前，
+             穿越人海到你身边，
+             等你出现，窥见彩虹，
+             傅菁，毛毛哒',
     },
     communityData: {
-      fireCount: 666,
-      commentCount: 998
+      fireCount: 157,
+      commentCount: 32
     }
   }, {
-    id: '2',
+    id: '3',
     articleType: 'picture',
     metaData: {
       authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
@@ -36,27 +39,27 @@ export default {
       commentCount: 998
     }
   }, {
-    id: '3',
+    id: '2',
     articleType: 'pictures',
     metaData: {
-      authorAvatar: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
-      authorName: 'John Lee',
-      timestamp: '一小时前',
+      authorAvatar: 'http://pic.qqtn.com/up/2017-11/2017112315111750386.jpg',
+      authorName: 'lele小仙女',
+      timestamp: '二小时前',
       location: '知春路'
     },
     articleData: {
-      text: '十号线地铁里竟然有个女生在抽烟，有个大爷提醒她，女生居然还大骂大爷...大家怎么看？十号线地铁里竟然有个女生在抽烟，大家怎么看？',
+      text: '我滴妈呀，太可爱辣！海獭宝宝明日花搓脸前还拍拍小手，要大干一番的架势~~~( • ̀ω•́ )完全萌的犯规啊……~真的好奇他们这么可爱的小脑袋里在想什么',
       pictures: [{
-        src: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
+        src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534163563175&di=464cdfa6f7a7f053a2e6810d39c3b740&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fvideo%2F86%2F86476a1d988a5cf190164c45b842c4d4.jpg'
       }, {
-        src: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
+        src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534163623360&di=292cac2c3d58c2d92b7bfaab25091304&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20170911%2F94e0e324f9cf4770883dde2dc560fdff.jpeg'
       }, {
-        src: 'https://avatars0.githubusercontent.com/u/17026165?s=400&v=4'
+        src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534163650172&di=8fb14191f50a6d42148098663cc431e2&imgtype=0&src=http%3A%2F%2Fp8.qhimg.com%2Ft01886b53bc9af2cd14.jpg%3Fsize%3D600x399'
       }]
     },
     communityData: {
-      fireCount: 666,
-      commentCount: 998
+      fireCount: 12345,
+      commentCount: 678
     }
   }, {
     id: '4',
