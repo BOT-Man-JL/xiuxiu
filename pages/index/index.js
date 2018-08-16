21a443e08599a461cccbcbe6f721b813c86197f0
BOT-Man-JL <g123nn@hotmail.com>
2018-08-16 23:28:16 +0800

    Support prompt unsupported function

--- pages/index/index.js
+++ pages/index/index.js
@@ -38,6 +38,13 @@ Page({
       })
     }, 500)
   },
+  promptUnsupported(e) {
+    wx.showModal({
+      title: config.appName,
+      content: e.currentTarget.dataset.text,
+      showCancel: false
+    })
+  },
   triggerFire(e) {
     const articleList = this.data.articleList
     const index = this.findIndexById(e.currentTarget.dataset.id)
