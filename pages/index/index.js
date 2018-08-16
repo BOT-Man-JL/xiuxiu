4eeb674e15fae09867f733cea5b2a10f7a69f173
BOT-Man-JL <g123nn@hotmail.com>
2018-08-16 10:08:05 +0800

    Support popup image

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,40 @@ import mockData from '../../resources/home/mock-data.js'
 const app = getApp()
 
 Page({
-  data: mockData,
+  data: Object.assign({
+    popupState: 'hide',
+    bigImageSrc: ''
+  }, mockData),
+  tempData: {
+    isPopupShown: false
+  },
   onLoad() {
     console.log('onloaded')
+  },
+  viewBigImage(e) {
+    this.setData({
+      bigImageSrc: e.target.dataset.src,
+      popupState: ''
+    })
+    this.tempData.isPopupShown = true
+  },
+  hideBigImage(e) {
+    if (!this.tempData.isPopupShown)
+      return
+
+    this.setData({
+      popupState: 'fade'
+    })
+    this.tempData.isPopupShown = false
+
+    // delay hiding to avoid 'display:none' issue
+    setTimeout(() => {
+      if (this.tempData.isPopupShown)
+        return
+
+      this.setData({
+        popupState: 'hide'
+      })
+    }, 500)
   }
 })
