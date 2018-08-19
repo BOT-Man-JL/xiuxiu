65bf2cfc71e7c88c0731942109ee99cea5a12137
BOT-Man-JL <g123nn@hotmail.com>
2018-08-19 17:14:13 +0800

    Add circle page

---
+++ pages/circle/circle.js
@@ -0,0 +1,8 @@
+//circle.js
+
+import config from '../../resources/config'
+import mockData from '../../resources/circle/mock-data.js'
+
+Page({
+  data: Object.assign({}, mockData)
+})
---
+++ resources/circle/mock-data.js
@@ -0,0 +1,6 @@
+export default {
+  pageData: {
+    followedCirclesTitle: '我关注的',
+    findingCirclesTitle: '发现圈子'
+  }
+}
