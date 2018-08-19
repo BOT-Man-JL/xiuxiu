d2e8dc5b82509377d14add9fba7147fda2a2e8ee
BOT-Man-JL <g123nn@hotmail.com>
2018-08-19 18:29:43 +0800

    Add swiper to circle page

--- resources/circle/mock-data.js
+++ resources/circle/mock-data.js
@@ -1,6 +1,21 @@
 export default {
   pageData: {
     followedCirclesTitle: '我关注的',
-    findingCirclesTitle: '发现圈子'
-  }
+    findingCirclesTitle: '发现圈子',
+    followText: '关注',
+    updateText: '更新'
+  },
+  followedCircleList: [{
+    id: '1',
+    name: '希格玛大厦',
+    image: 'https://s1.ax1x.com/2018/08/13/PgrjeO.jpg',
+    followCount: '14万',
+    updateCount: '18'
+  }, {
+    id: '2',
+    name: '六道口',
+    image: 'https://n1image.hjfile.cn/mh/2017/06/26/9ffa8c56cfd76cf5159011f4017f022e.jpg',
+    followCount: '228',
+    updateCount: '13'
+  }]
 }
