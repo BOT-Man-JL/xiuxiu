47ab3c3e318aaceab885bbe181cd5264662d953f
BOT-Man-JL <g123nn@hotmail.com>
2018-08-28 09:44:19 +0800

    format code

--- resources/article-data.js
+++ resources/article-data.js
@@ -4,84 +4,84 @@ export default {
   },
   locationText: '希格玛大厦',
   articleList: [{
-      id: '1',  
-      articleType: 'picture',  
+      id: '1',
+      articleType: 'picture',
       metaData: {
-          authorAvatar: 'http://img1.lukou.com/static/p/blog/medium/0012/45/32/97/12453297.jpg',  
-          authorName: '希格玛小可爱',  
-          timestamp: '15分钟前',  
-          circle: '希格玛大厦', 
-          circleId: '1'
+        authorAvatar: 'http://img1.lukou.com/static/p/blog/medium/0012/45/32/97/12453297.jpg',
+        authorName: '希格玛小可爱',
+        timestamp: '15分钟前',
+        circle: '希格玛大厦',
+        circleId: '1'
       },
       articleData: {
-          text: '101小姐姐来希格玛了，就在四楼，快去看啊！',  
-          picture: {  
-            src: 'http://n.sinaimg.cn/sinacn12/107/w1024h683/20180817/2104-hhvciiw5404397.jpg'
-          }
+        text: '101小姐姐来希格玛了，就在四楼，快去看啊！',
+        picture: {
+          src: 'http://n.sinaimg.cn/sinacn12/107/w1024h683/20180817/2104-hhvciiw5404397.jpg'
+        }
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 101,  
-          commentCount: 37  
+        hasFire: false,
+        fireCount: 101,
+        commentCount: 37
       }
     },
     {
-      id: '2',  
-      articleType: 'picture',  
+      id: '2',
+      articleType: 'picture',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/22/Poz22q.jpg',  
-          authorName: '新京报',  
-          timestamp: '15分钟前',  
-          location: '中国人民大学'  
+        authorAvatar: 'https://s1.ax1x.com/2018/08/22/Poz22q.jpg',
+        authorName: '新京报',
+        timestamp: '15分钟前',
+        location: '中国人民大学'
       },
       articleData: {
-          text: '中国人民大学一栋宿舍楼突发火灾。学生称，起火的为人大红三宿舍楼，主要是研究生宿舍，主要位于顶楼，“大火几乎将楼顶烧没了，不过，现在该楼二楼、三楼都没有学生居住。”目前大火已扑灭，无人员伤亡。',  
-          picture: {  
-              src: 'https://s1.ax1x.com/2018/08/22/Poxqu8.jpg'
-          }
+        text: '中国人民大学一栋宿舍楼突发火灾。学生称，起火的为人大红三宿舍楼，主要是研究生宿舍，主要位于顶楼，“大火几乎将楼顶烧没了，不过，现在该楼二楼、三楼都没有学生居住。”目前大火已扑灭，无人员伤亡。',
+        picture: {
+          src: 'https://s1.ax1x.com/2018/08/22/Poxqu8.jpg'
+        }
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 93,  
-          commentCount: 21  
+        hasFire: false,
+        fireCount: 93,
+        commentCount: 21
       }
     },
     {
-      id: '3',  
-      articleType: 'news',  
+      id: '3',
+      articleType: 'news',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/24/P7X810.jpg',  
-          authorName: '北京日报',  
-          timestamp: '刚刚',  
-          location: '龙泉寺'  
+        authorAvatar: 'https://s1.ax1x.com/2018/08/24/P7X810.jpg',
+        authorName: '北京日报',
+        timestamp: '刚刚',
+        location: '龙泉寺'
       },
       articleData: {
-          text: '【快讯】国家宗教局：举报材料中反映北京龙泉寺住持学诚发送骚扰信息问题，经查属实。',  
+        text: '【快讯】国家宗教局：举报材料中反映北京龙泉寺住持学诚发送骚扰信息问题，经查属实。',
       },
       communityData: {
-          hasFire: false,  
-          fireCount: '1.5万',  
-          commentCount: '4万'  
+        hasFire: false,
+        fireCount: '1.5万',
+        commentCount: '4万'
       }
     },
     {
-      id: '4',  
-      articleType: 'pictures',  
+      id: '4',
+      articleType: 'pictures',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/24/P7X2He.jpg',  
-          authorName: '北京事儿',  
-          timestamp: '6小时前',  
-          location: '北京'  
+        authorAvatar: 'https://s1.ax1x.com/2018/08/24/P7X2He.jpg',
+        authorName: '北京事儿',
+        timestamp: '6小时前',
+        location: '北京'
       },
       articleData: {
-          text: '一个外国人在1990年用胶片相机拍下的北京亚运会时的街景，感受一下。',  
-          pictures: [{  
-              src: 'https://s1.ax1x.com/2018/08/24/P7Xr1x.jpg'
-            },{
-              src: 'https://s1.ax1x.com/2018/08/24/P7Xsc6.jpg'
-            },{
-              src: 'https://s1.ax1x.com/2018/08/24/P7XgBD.jpg'
-          }]
+        text: '一个外国人在1990年用胶片相机拍下的北京亚运会时的街景，感受一下。',
+        pictures: [{
+          src: 'https://s1.ax1x.com/2018/08/24/P7Xr1x.jpg'
+        }, {
+          src: 'https://s1.ax1x.com/2018/08/24/P7Xsc6.jpg'
+        }, {
+          src: 'https://s1.ax1x.com/2018/08/24/P7XgBD.jpg'
+        }]
       },
       communityData: {
         hasFire: false,
@@ -90,170 +90,170 @@ export default {
       }
     },
     {
-      id: '5',  
-      articleType: 'picture',  
+      id: '5',
+      articleType: 'picture',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/24/P7XZX8.jpg',  
-          authorName: '百香果的夏天',  
-          timestamp: '5分钟前',  
-          location: '知春路'  
+        authorAvatar: 'https://s1.ax1x.com/2018/08/24/P7XZX8.jpg',
+        authorName: '百香果的夏天',
+        timestamp: '5分钟前',
+        location: '知春路'
       },
       articleData: {
-          text: '爱就像蓝天白云，晴空万里。感受下北京今日的蓝天吧，雾霾治理初显成效！',  
-          picture: {  
-              src: 'https://s1.ax1x.com/2018/08/24/P7XMkj.jpg'
-          }
+        text: '爱就像蓝天白云，晴空万里。感受下北京今日的蓝天吧，雾霾治理初显成效！',
+        picture: {
+          src: 'https://s1.ax1x.com/2018/08/24/P7XMkj.jpg'
+        }
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 23,  
-          commentCount: 10  
+        hasFire: false,
+        fireCount: 23,
+        commentCount: 10
       }
     },
     {
-      id: '6',  
-      articleType: 'picture',  
+      id: '6',
+      articleType: 'picture',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/24/PHul1P.jpg',  
-          authorName: '妖精之吻',  
-          timestamp: '1小时前',  
-          circle: '故宫博物馆', 
-          circleId: '5'
+        authorAvatar: 'https://s1.ax1x.com/2018/08/24/PHul1P.jpg',
+        authorName: '妖精之吻',
+        timestamp: '1小时前',
+        circle: '故宫博物馆',
+        circleId: '5'
       },
       articleData: {
-          text: '外地牌照车辆于8月24日周五18:20左右进入西二环主路行使，并违反尾号限行措施，并在晚高峰时期进入五环内！',  
-          picture: {  
-              src: 'https://s1.ax1x.com/2018/08/24/PHu16f.jpg'
-          }
+        text: '外地牌照车辆于8月24日周五18:20左右进入西二环主路行使，并违反尾号限行措施，并在晚高峰时期进入五环内！',
+        picture: {
+          src: 'https://s1.ax1x.com/2018/08/24/PHu16f.jpg'
+        }
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 2,  
-          commentCount: 4  
+        hasFire: false,
+        fireCount: 2,
+        commentCount: 4
       }
     },
     {
-      id: '7',  
-      articleType: 'picture',  
+      id: '7',
+      articleType: 'picture',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/27/PqhFv4.jpg',  
-          authorName: '魔芋打人',  
-          timestamp: '1小时前',  
-          circle: '清华大学', 
-          circleId: '4'  
+        authorAvatar: 'https://s1.ax1x.com/2018/08/27/PqhFv4.jpg',
+        authorName: '魔芋打人',
+        timestamp: '1小时前',
+        circle: '清华大学',
+        circleId: '4'
       },
       articleData: {
-          text: 'AI魔芋翻译机出现在清华大学创新展示中心，周边的小伙伴可以去体验下。',  
-          picture: {  
-              src: 'https://s1.ax1x.com/2018/08/27/PqhVbR.jpg'
-          }
+        text: 'AI魔芋翻译机出现在清华大学创新展示中心，周边的小伙伴可以去体验下。',
+        picture: {
+          src: 'https://s1.ax1x.com/2018/08/27/PqhVbR.jpg'
+        }
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 56,  
-          commentCount: 24  
+        hasFire: false,
+        fireCount: 56,
+        commentCount: 24
       }
     },
     {
-      id: '8',  
-      articleType: 'news',  
+      id: '8',
+      articleType: 'news',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/22/Poz22q.jpg ',  
-          authorName: '新京报',  
-          timestamp: '1小时前',  
-          location: '北京'  
+        authorAvatar: 'https://s1.ax1x.com/2018/08/22/Poz22q.jpg ',
+        authorName: '新京报',
+        timestamp: '1小时前',
+        location: '北京'
       },
       articleData: {
-          text: '【重磅】取消文理科，本科一、二批次合并，北京市“新高考”方案来啦！北京高考录取拟于2019年合并本科一、二批次，针对学生等级性考试成绩纳入高考成绩的“折算赋分方案”也于日前出台。北京高考录取拟于2019年合并本科一、二批次，针对学生等级性考试成绩纳入高考成绩的“折算赋分方案”也于日前出台。'
+        text: '【重磅】取消文理科，本科一、二批次合并，北京市“新高考”方案来啦！北京高考录取拟于2019年合并本科一、二批次，针对学生等级性考试成绩纳入高考成绩的“折算赋分方案”也于日前出台。北京高考录取拟于2019年合并本科一、二批次，针对学生等级性考试成绩纳入高考成绩的“折算赋分方案”也于日前出台。'
       },
       communityData: {
-          hasFire: false,  
-          fireCount: '2万',  
-          commentCount: '1.3万'  
+        hasFire: false,
+        fireCount: '2万',
+        commentCount: '1.3万'
       }
     },
     {
-      id: '9',  
-      articleType: 'text',  
+      id: '9',
+      articleType: 'text',
       metaData: {
-          authorAvatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306273419&di=ab12600e16f7c049dbdbccec0ebc9114&imgtype=0&src=http%3A%2F%2Fimg5q.duitang.com%2Fuploads%2Fitem%2F201502%2F06%2F20150206172230_eYKQT.thumb.700_0.jpeg',  
-          authorName: '梁斌penny',  
-          timestamp: '6小时前',  
-          circle: '五道口', 
-          circleId: '2'  
+        authorAvatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306273419&di=ab12600e16f7c049dbdbccec0ebc9114&imgtype=0&src=http%3A%2F%2Fimg5q.duitang.com%2Fuploads%2Fitem%2F201502%2F06%2F20150206172230_eYKQT.thumb.700_0.jpeg',
+        authorName: '梁斌penny',
+        timestamp: '6小时前',
+        circle: '五道口',
+        circleId: '2'
       },
       articleData: {
-          text: '不要去北京五道口的“信服”维修任何设备！这是一家黑店！我进去买手机没有合适的，他们还骂骂咧咧不让我走，言语有威胁，我声称报警，他们才放弃了。',  
+        text: '不要去北京五道口的“信服”维修任何设备！这是一家黑店！我进去买手机没有合适的，他们还骂骂咧咧不让我走，言语有威胁，我声称报警，他们才放弃了。',
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 123,  
-          commentCount: 21  
+        hasFire: false,
+        fireCount: 123,
+        commentCount: 21
       }
     },
     {
       id: '10',
       articleType: 'text',
       metaData: {
-          authorAvatar: 'https://s1.ax1x.com/2018/08/25/PHof0A.jpg',
-          authorName: '千龙网',
-          timestamp: '刚刚',
-          circle: '融泽嘉园', 
-          circleId: '3'
+        authorAvatar: 'https://s1.ax1x.com/2018/08/25/PHof0A.jpg',
+        authorName: '千龙网',
+        timestamp: '刚刚',
+        circle: '融泽嘉园',
+        circleId: '3'
       },
       articleData: {
-          text: '近日接到市民举报，融泽嘉园北面的工地天天半夜还在施工，扰乱了附近住户的正常休息。昌平区回龙观镇政府回复：融泽嘉园小区北侧为京张高铁工地，施工单位为中铁七局、中铁六局，正在进行修建高铁作业。对此，执法人员找到现场负责人，要求其加强施工现场管理，并合理安排工期，杜绝夜间施工行为。',
+        text: '近日接到市民举报，融泽嘉园北面的工地天天半夜还在施工，扰乱了附近住户的正常休息。昌平区回龙观镇政府回复：融泽嘉园小区北侧为京张高铁工地，施工单位为中铁七局、中铁六局，正在进行修建高铁作业。对此，执法人员找到现场负责人，要求其加强施工现场管理，并合理安排工期，杜绝夜间施工行为。',
       },
       communityData: {
-          hasFire: false,
-          fireCount: 873,
-          commentCount: 58
+        hasFire: false,
+        fireCount: 873,
+        commentCount: 58
       }
     },
     {
-      id: '11',  
-      articleType: 'pictures',  
+      id: '11',
+      articleType: 'pictures',
       metaData: {
-          authorAvatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535305796801&di=9d69779247641f8daca52975552ade65&imgtype=0&src=http%3A%2F%2Fimg18.3lian.com%2Fd%2Ffile%2F201709%2F08%2Fc4b6815edcad535afdf16471feb54b84.jpg',  
-          authorName: '北京吃喝玩乐',  
-          timestamp: '昨天',  
-          circle: '五道口', circleId: 2  
+        authorAvatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535305796801&di=9d69779247641f8daca52975552ade65&imgtype=0&src=http%3A%2F%2Fimg18.3lian.com%2Fd%2Ffile%2F201709%2F08%2Fc4b6815edcad535afdf16471feb54b84.jpg',
+        authorName: '北京吃喝玩乐',
+        timestamp: '昨天',
+        circle: '五道口',
+        circleId: 2
       },
       articleData: {
-          text: '五道口华联新开了一家性价比超高的萨莉亚。点了两个小披萨都很好吃，没有很厚的边，脆脆的很好吃。其他的烤鸡翅和烤饭也很实惠。没想到的是在一家西餐厅竟然爱上了裙带菜。不过金枪鱼汤面不推荐，有点腥。五道口这家萨莉亚外国人超多，坐久了感觉自己才是外国人。',
-          pictures: [{  
-              src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306515855&di=9b7759f70a752c32ccca2519cdea0a2d&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01b09d5930e569a8012193a3c3f923.JPG%401280w_1l_2o_100sh.jpg'
-            },{
-              src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306515855&di=235084a72b81c194a37b72219ae887c7&imgtype=0&src=http%3A%2F%2Fuploads.xuexila.com%2Fallimg%2F1504%2F631-150416164951A8.jpg'
-            },{
-              src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306515855&di=ea6c5ce84c272f11e89801402d7b8cf5&imgtype=0&src=http%3A%2F%2Fimage-swws.258.com%2F1%2F2018%2F0120%2F09%2F5a62a1baf2d0c.jpg'
-            }]
+        text: '五道口华联新开了一家性价比超高的萨莉亚。点了两个小披萨都很好吃，没有很厚的边，脆脆的很好吃。其他的烤鸡翅和烤饭也很实惠。没想到的是在一家西餐厅竟然爱上了裙带菜。不过金枪鱼汤面不推荐，有点腥。五道口这家萨莉亚外国人超多，坐久了感觉自己才是外国人。',
+        pictures: [{
+          src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306515855&di=9b7759f70a752c32ccca2519cdea0a2d&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01b09d5930e569a8012193a3c3f923.JPG%401280w_1l_2o_100sh.jpg'
+        }, {
+          src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306515855&di=235084a72b81c194a37b72219ae887c7&imgtype=0&src=http%3A%2F%2Fuploads.xuexila.com%2Fallimg%2F1504%2F631-150416164951A8.jpg'
+        }, {
+          src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535306515855&di=ea6c5ce84c272f11e89801402d7b8cf5&imgtype=0&src=http%3A%2F%2Fimage-swws.258.com%2F1%2F2018%2F0120%2F09%2F5a62a1baf2d0c.jpg'
+        }]
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 46,  
-          commentCount: 34  
+        hasFire: false,
+        fireCount: 46,
+        commentCount: 34
       }
     },
     {
-      id: '12',  
-      articleType: 'text',  
+      id: '12',
+      articleType: 'text',
       metaData: {
-          authorAvatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535360034483&di=b744c2584541d3f936b2401965e987e8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F472309f790529822eae390e7ddca7bcb0b46d4c6.jpg',  
-          authorName: '匿名',  
-          timestamp: '2小时前', 
-          circle: '希格玛大厦', 
-          circleId: '1'  
+        authorAvatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535360034483&di=b744c2584541d3f936b2401965e987e8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F472309f790529822eae390e7ddca7bcb0b46d4c6.jpg',
+        authorName: '匿名',
+        timestamp: '2小时前',
+        circle: '希格玛大厦',
+        circleId: '1'
       },
       articleData: {
-          text: '中午去希格玛大厦楼下取外卖，在找外卖小哥的时候，冷不丁被唾沫星子喷了半边脸，只见旁边一男的朝快递小哥破口大骂（30多岁，方脸，皮肤略黑，带眼镜，身高一米八多，穿墨绿色冲锋衣）限制级的脏话一句接一句，声音大得街对面都能听到，足足骂了三分钟，而且是在快递小哥没有回嘴的情况下！“你没到打什么电话？！caonm的，**玩意……！”',  
+        text: '中午去希格玛大厦楼下取外卖，在找外卖小哥的时候，冷不丁被唾沫星子喷了半边脸，只见旁边一男的朝快递小哥破口大骂（30多岁，方脸，皮肤略黑，带眼镜，身高一米八多，穿墨绿色冲锋衣）限制级的脏话一句接一句，声音大得街对面都能听到，足足骂了三分钟，而且是在快递小哥没有回嘴的情况下！“你没到打什么电话？！caonm的，**玩意……！”',
       },
       communityData: {
-          hasFire: false,  
-          fireCount: 132,  
-          commentCount:72  
+        hasFire: false,
+        fireCount: 132,
+        commentCount: 72
       }
     }
   ]
 }
-
